d8fce261232f48e964267a92b8cb638cd56018ba
Oleg Nechaev <lega911@gmail.com>
2015-11-10 01:54:19 +0500

    Angular.js, jQuery, JavaScript

--- ENV.js
+++ ENV.js
@@ -22,7 +22,7 @@
         document.body.insertBefore(div, document.body.firstChild);
 
         function setResult(fill, update) {
-            div.innerHTML = 'Fill: <b>' + fill + 'ms</b>, Update: <b>' + update + 'ms</b>';
+            div.innerHTML = 'Fill: <b>' + fill.toFixed(0) + 'ms</b>, Update: <b>' + update.toFixed(0) + 'ms</b>';
         }
 
         var count = 10000;
@@ -34,21 +34,31 @@
             }
         };
 
+        var total = {
+            fill: 0,
+            update: 0,
+            count: 0
+        };
+
         function next() {
             app.clear(nextTick(500, function() {
                 var start = time();
                 app.fill(count, nextTick(0, function() {
-                    var fillDuration = (time() - start).toFixed(0);
+                    var fillDuration = time() - start;
                     setResult(fillDuration, 0);
 
                     nextTick(1000, function() {
                         start = time();
                         app.update(count, nextTick(0, function() {
-                            var updateDuration = (time() - start).toFixed(0);
+                            var updateDuration = time() - start;
                             setResult(fillDuration, updateDuration);
 
-                            localStorage.setItem('fill:' + app.code, fillDuration);
-                            localStorage.setItem('update:' + app.code, updateDuration);
+                            total.fill += fillDuration;
+                            total.update += updateDuration;
+                            total.count++
+
+                            localStorage.setItem('fill:' + app.code, (total.fill/total.count).toFixed(0));
+                            localStorage.setItem('update:' + app.code, (total.update/total.count).toFixed(0));
                             nextTick(2000, next)();
                         }));
                     })();
--- library-list.js
+++ library-list.js
@@ -1,8 +1,11 @@
 ENV.libraryList = [
     { code: 'angular-light', name: 'Angular Light', path: './angular-light/' },
     { code: 'angularjs', name: 'Angular.js', path: './angularjs/' },
+    { code: 'jquery', name: 'jQuery', path: './jquery/' },
+    { code: 'js', name: 'JavaScript', path: './javascript/' },
+    { code: 'js2', name: 'JavaScript + DocumentFragment', path: './javascript/v2.html' },
+
     { name: 'Matreshka.js', path: './matreshka/' },
-    { name: 'jQuery', path: './jquery/' },
     { name: 'Knockout.js', path: './knockoutjs/' },
     { name: 'React.js', path: './reactjs/' }
 ];
