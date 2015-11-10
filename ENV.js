dcbc7e5dfb9c5057a79371aa0a8d7bec521cdcc5
Oleg Nechaev <lega911@gmail.com>
2015-11-10 23:43:46 +0500

    use requestAnimationFrame

--- ENV.js
+++ ENV.js
@@ -1,4 +1,3 @@
-
 (function(){
     window.ENV = {};
     document.addEventListener('DOMContentLoaded', run);
@@ -11,21 +10,33 @@
     function time() {
         if(performance) return performance.now()
         return Date.now()
-    }
+    };
+
+    function buildBoard() {
+        var div = document.createElement('div');
+        document.body.insertBefore(div, document.body.firstChild);
+        var fillResult=0, updateResult=0;
+        displayResult();
+
+        function displayResult(fill, update) {
+            if(fill) fillResult = fill;
+            if(update) updateResult = update;
+            div.innerHTML = 'Fill: <b>' + fillResult.toFixed(0) + 'ms</b>, Update: <b>' + updateResult.toFixed(0) + 'ms</b>';
+        }
+
+        return displayResult;
+    };
 
     function run() {
         if(!app) return;
-
-        var div = document.createElement('div');
-        setResult(0, 0);
-
-        document.body.insertBefore(div, document.body.firstChild);
-
-        function setResult(fill, update) {
-            div.innerHTML = 'Fill: <b>' + fill.toFixed(0) + 'ms</b>, Update: <b>' + update.toFixed(0) + 'ms</b>';
-        }
+        var displayResult = buildBoard();
+        var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                                    window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
         var count = 10000;
+        var fillDuration, updateDuration;
+        var total = { fill: 0, update: 0, count: 0 };
+
         next();
 
         function nextTick(delay, fn) {
@@ -34,36 +45,45 @@
             }
         };
 
-        var total = {
-            fill: 0,
-            update: 0,
-            count: 0
+        function takeFill(callback) {
+            var start = time();
+            app.fill(count, nextTick(0, function() {
+                fillDuration = time() - start;
+                displayResult(fillDuration, 0);
+                callback()
+            }))
+        }
+
+        function takeUpdate(callback) {
+            var start = time();
+            app.update(count, nextTick(0, function() {
+                updateDuration = time() - start;
+                displayResult(0, updateDuration);
+                callback()
+            }));
+        }
+
+        function saveResult() {
+            total.fill += fillDuration;
+            total.update += updateDuration;
+            total.count++
+
+            localStorage.setItem('fill:' + app.code, (total.fill/total.count).toFixed(0));
+            localStorage.setItem('update:' + app.code, (total.update/total.count).toFixed(0));
         };
 
         function next() {
             app.clear(nextTick(500, function() {
-                var start = time();
-                app.fill(count, nextTick(0, function() {
-                    var fillDuration = time() - start;
-                    setResult(fillDuration, 0);
-
-                    nextTick(1000, function() {
-                        start = time();
-                        app.update(count, nextTick(0, function() {
-                            var updateDuration = time() - start;
-                            setResult(fillDuration, updateDuration);
-
-                            total.fill += fillDuration;
-                            total.update += updateDuration;
-                            total.count++
-
-                            localStorage.setItem('fill:' + app.code, (total.fill/total.count).toFixed(0));
-                            localStorage.setItem('update:' + app.code, (total.update/total.count).toFixed(0));
-                            nextTick(2000, next)();
-                        }));
-                    })();
-
-                }))
+                requestAnimationFrame(function() {
+                    takeFill(nextTick(1000, function() {
+                        requestAnimationFrame(function() {
+                            takeUpdate(function() {
+                                saveResult();
+                                setTimeout(next, 2000)
+                            })
+                        })
+                    }));
+                })
             }))
         }
     }
