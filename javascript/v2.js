aeaf1a2f228beec4dcf18c7e084de9da29b717f0
Oleg Nechaev <lega911@gmail.com>
2015-11-12 01:18:52 +0500

    fix JavaScript + DocumentFragment

--- javascript/v2.js
+++ javascript/v2.js
@@ -1,42 +1,43 @@
 'use strict';
 
 $(function() {
-  var root = $("#test-area")[0];
-  var ul = document.createElement('ul');
-  var list = [];
+    var root = $("#test-area")[0];
+    var ul = document.createElement('ul');
+    root.appendChild(ul);
+    var list = [];
 
-  ENV.append({
-      code: 'js2',
-      clear: clear,
-      fill: fill,
-      update: update
-  })
-
-  function clear(callback) {
-      ul.innerHTML = '';
-      list.length = 0
-      callback();
-  };
-  
-  function fill(n, callback) {
-      var i, li, text;
-      for (i = 0; i < n; i++) {
+    ENV.append({
+        code: 'js2',
+        clear: clear,
+        fill: fill,
+        update: update
+    })
+    
+    function clear(callback) {
+        ul.innerHTML = '';
+        list.length = 0;
+        callback()
+    };
+    
+    function fill(n, callback) {
+        var i, li, text, fragment = document.createDocumentFragment();;
+        for (i = 0; i < n; i++) {
           li = document.createElement('li');
           text = document.createTextNode('jj: ' + i);
           li.appendChild(text);
-          ul.appendChild(li);
+          fragment.appendChild(li);
           list.push(text);
-      };
-      root.appendChild(ul);
-      callback();
-  };
-  
-  function update(n, callback) {
-      var i;
-      for (i = 0; i < n; i++) {
-          list[i].textContent = 'jj: ' + i + ' ' + i;
-      };
-      callback();
-  }
+        };
+        ul.appendChild(fragment);
+        callback()
+    };
+    
+    function update(n, callback) {
+        var i;
+        for (i = 0; i < n; i++) {
+            list[i].textContent = 'jj: ' + i + ' ' + i;
+        };
+        callback()
+    }
 
 })
